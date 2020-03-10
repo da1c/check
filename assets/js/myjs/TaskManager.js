aac39d633890942873c77b9c809ed84894b14c0d
da1c <axda1c21@gmail.com>
2020-03-10 16:07:21 +0900

    Add files via upload

---
+++ assets/js/myjs/TaskManager.js
@@ -0,0 +1,74 @@
+/**
+ * タスク管理クラス
+ * @class TaskManager
+ */
+class TaskManager{
+
+    /**
+     * タスクリスト
+     * @static
+     * @memberof TaskManager
+     */
+    taskArray;
+
+    
+    /**
+     *Creates an instance of TaskManager.
+     * @memberof TaskManager
+     */
+    constructor(){
+        TaskManager.instance = this;
+    }
+
+    /**
+     * 初期化処理
+     * 
+     * @static
+     * @memberof TaskManager
+     */
+    Init(){
+        // タスクリストのメモリ
+        this.taskArray = new Array();
+    }
+
+    // task登録
+
+    /**
+     *
+     * @static
+     * @param {*} registTask
+     * @memberof TaskManager
+     */
+    Regist( registTask){
+        // リストにタスク追加
+        this.taskArray.push(registTask);
+    }
+
+    // Task更新
+
+    /**
+     *
+     *
+     * @memberof TaskManager
+     */
+    Update(){
+
+        this.taskArray.forEach(element => {
+            // Task毎の更新処理
+            element.Update();
+        });
+
+    }
+
+    /**
+     * Task削除
+     *
+     * @memberof TaskManager
+     */
+    Delete(){
+
+    }
+}
+
+// インスタンス定義
+TaskManager.instance = null;
